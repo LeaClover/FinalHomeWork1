7c8111330e10dde2472607aff804056a9a2ce78b
LeaClover <plesen_14@mail.ru>
2022-08-10 20:01:11 +0300

    editting project file program hw

--- FinalHomeWork1/Program.cs
+++ FinalHomeWork1/Program.cs
@@ -12,29 +12,32 @@ namespace finalHomeWorkGB
             string? str = Console.ReadLine();
             string[] array = str.Split(' ');
             Methods method = new Methods();
+            text.TextResult();
             method.PrintArray(method.GetArrayWhereStringLess3(array));
         }
     }
 
     class Methods
     {
-        public string [] GetArrayWhereStringLess3(string[] array)
+        public string[] GetArrayWhereStringLess3(string[] array)
         {
             string[] arrayS = new string[array.Length];
+            int index = 0;
             for (int i = 0; i < array.Length; i++)
             {
                 string str = array[i];
                 int size = str.Length;
                 if (size <= 3)
                 {
-                    arrayS[i] = array[i];
+                    arrayS[index] = array[i];
+                    index++;
                 }
             }
             return arrayS;
         }
-        public void PrintArray(string [] arrayP)
+        public void PrintArray(string[] arrayP)
         {
-            for(int i = 0; i < arrayP.Length; i++)
+            for (int i = 0; i < arrayP.Length; i++)
             {
                 Console.Write(arrayP[i] + " ");
             }
@@ -44,11 +47,15 @@ namespace finalHomeWorkGB
     {
         public void MainText()
         {
-            Console.WriteLine("Программа формирует массив строк, где количество символов меньше или равно трем!")
+            Console.WriteLine("Программа формирует массив строк, где количество символов меньше или равно трем!");
         }
         public void TextToDo()
         {
             Console.WriteLine("Введите текст через пробел и мы сформируем массив");
+        }
+        public void TextResult()
+        {
+            Console.WriteLine("Сформирован итоговый массив, где у элементов количество символов меньше трех: ");
         }
     }
 }
